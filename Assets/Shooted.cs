4e2fe7430a7ca79a499c786ac19eb26b119e82ee
Jorge <joranboc@hotmail.com>
2017-01-17 18:45:35 -0500

    Tutorial

---
+++ Assets/Shooted.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooted : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter (Collision col){
+		if(col.gameObject.CompareTag("Wood")){
+			
+		}else{
+			Debug.Log("shit me dieron");
+		}
+	}
+}
